aadc786200a4bdfac6ca26184a33c16d72176b9b
MarkusIngerslev <markusolsen2004@gmail.com>
2023-05-01 19:36:39 +0200

    added funktionalitet til at ting fra firebase database nu bliver vist i grids på siden

--- app.js
+++ app.js
@@ -1,19 +1,66 @@
 "use strict";
+// ========== Globale variables ========== //
 
-window.addEventListener("load", startApp);
-
-const endpoint = "https://movies-forms-rest-crud-afl-default-rtdb.europe-west1.firebasedatabase.app/";
+const endpoint =
+  "https://movies-forms-rest-crud-afl-default-rtdb.europe-west1.firebasedatabase.app/";
 
 let movies;
 
+// ========== Load & startup ========== //
+window.addEventListener("load", startApp);
+
 function startApp() {
-  movies = getMovies();
-  console.log(movies);
+  // update the grid of movies: get and show all movies
+  updateMovieGrid();
 }
 
+// ========== READ ========== //
 async function getMovies() {
   const response = await fetch(`${endpoint}/movies.json`);
   const data = await response.json();
 
-  return data;
+  const movies = prepareMovieData(data);
+  return movies;
+}
+
+// ========== Update Function ========== //
+async function updateMovieGrid() {
+  document.querySelector("#movies").innerHTML = ""; // tømmer hele div'en for movie elementer
+  movies = await getMovies();
+
+  showMovies(movies);
+}
+
+// ========== HTML opsætning ========== //
+
+function showMovies(movieList) {
+  for (const movie of movieList) {
+    showMovie(movie);
+  }
+}
+
+function showMovie(movieObject) {
+  document.querySelector("#movies").insertAdjacentHTML(
+    "beforeend",
+    /*html*/ `
+    <article>
+      <img src=${movieObject.image}>
+      <h2>${movieObject.title}</h2>
+      <p>${movieObject.description}</p>
+      <button class="btn-delete">Delete</button>
+      <button class="btn-update">Update</button>
+    </article>
+  `
+  );
+}
+
+// ========== Objekt til array ========== //
+function prepareMovieData(dataObjekt) {
+  const movieArray = [];
+  for (const key in dataObjekt) {
+    const movie = dataObjekt[key];
+    movie.id = key;
+    movieArray.push(movie);
+  }
+  return movieArray;
 }
