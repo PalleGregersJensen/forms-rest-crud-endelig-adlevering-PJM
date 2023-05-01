bd91fc6f6cd1a154d88a8236951e167afd4e250f
Jon <lundbyjon@hotmail.com>
2023-05-01 14:10:21 +0200

    fetching database added

--- app.js
+++ app.js
@@ -2,6 +2,18 @@
 
 window.addEventListener("load", startApp);
 
+const endpoint = "https://movies-forms-rest-crud-afl-default-rtdb.europe-west1.firebasedatabase.app/";
+
+let movies;
+
 function startApp() {
-  console.log("JS kører");
+  movies = getMovies();
+  console.log(movies);
+}
+
+async function getMovies() {
+  const response = await fetch(`${endpoint}/movies.json`);
+  const data = await response.json();
+
+  return data;
 }
