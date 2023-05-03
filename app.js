a63c9dc91e626c6f32b71edf9fcc86332b0881aa
MarkusIngerslev <markusolsen2004@gmail.com>
2023-05-03 19:39:58 +0200

    Ændret tilbage til rigtig firebase database

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 "use strict";
 // ========== Globale variables ========== //
 
-// const endpoint = "https://movies-forms-rest-crud-afl-default-rtdb.europe-west1.firebasedatabase.app/";
-const endpoint = "https://test-form-database-f322e-default-rtdb.firebaseio.com/";
+const endpoint = "https://movies-forms-rest-crud-afl-default-rtdb.europe-west1.firebasedatabase.app/";
+// const endpoint = "https://test-form-database-f322e-default-rtdb.firebaseio.com/";
 let movies;
 
 // ========== Load & startup ========== //
